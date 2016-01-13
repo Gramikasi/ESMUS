4e90576cdecd9df41d285e8e8aec274213e6aad2
Alejandro <alumno@U109021>
2016-01-13 16:58:00 +0100

    Register activity implemmentado

--- app/src/main/java/com/example/alejandro/esmus/RegisterActivivty.java
+++ app/src/main/java/com/example/alejandro/esmus/RegisterActivivty.java
@@ -8,12 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.alejandro.esmus.vista.AudioPlayer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class RegisterActivivty extends ModelActivity {
@@ -26,9 +29,9 @@ public class RegisterActivivty extends ModelActivity {
         setContentView(R.layout.activity_register_activivty);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ArrayList<String> login=new ArrayList<>();
         layout = (LinearLayout) findViewById(R.id.register_activity_linear);
-
+        login=pref.readPref();
         Toast.makeText(this,content.getContenido().toString(),Toast.LENGTH_SHORT).show();
         Toast.makeText(this, content.getTematicas().toString(), Toast.LENGTH_SHORT).show();
         Toast.makeText(this, Integer.valueOf(content.getExtraIndiceTematica()).toString(), Toast.LENGTH_SHORT).show();
@@ -47,7 +50,47 @@ public class RegisterActivivty extends ModelActivity {
 
             }
         }).start();
+        //logica mostrar registros
+        TextView textView=(TextView)findViewById(R.id.welcome_message_register);
 
+        textView.setText("Hola " + login.get(0) + " " + login.get(1) + " has venido a " + login.get(2) + " de visita!Quizas podria ayudarte a comunicarte en alguno de estos sitios!");
+
+        ArrayList<String> registros=content.getRegistros(content.getExtraIndiceTematica());
+
+        //ListView listView=(ListView)findViewById(R.id.listView);
+        //final ArrayList mLista = new ArrayList();
+        //final ArrayAdapter mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mLista);
+        //list.setAdapter(mAdapter);
+
+        final View.OnClickListener listener= new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int i= (int) v.getTag();
+                content.putExtraIndiceRegistro(i);
+                startModelActivity(RegisterActivivty.class);
+
+            }
+        };
+        LinearLayout list=(LinearLayout)findViewById(R.id.linearListview);
+        int i=0;
+        for (String registro : registros)
+        {
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            Button button=new Button(this);
+
+            button.setText(registro);
+            button.setTag(i);
+            button.setOnClickListener(listener);
+            button.setLayoutParams(params);
+            list.addView(button);
+
+            //  mLista.add(button);
+
+        }
 
 
     }
