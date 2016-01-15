d7293aa66ec15804efc78b95e5d1536359d666b4
Miren <mjcava001@ikasle.ehu.es>
2016-01-15 15:31:21 +0100

    implementando showAdviceActivity

--- app/src/main/java/com/example/alejandro/esmus/RegisterActivivty.java
+++ app/src/main/java/com/example/alejandro/esmus/RegisterActivivty.java
@@ -6,17 +6,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.alejandro.esmus.vista.AudioPlayer;
+import com.example.alejandro.esmus.vista.ProgressTask;
 
 import java.io.IOException;
 
 
 public class RegisterActivivty extends ModelActivity {
+
 
     private LinearLayout layout;
 
@@ -29,37 +32,12 @@ public class RegisterActivivty extends ModelActivity {
 
         layout = (LinearLayout) findViewById(R.id.register_activity_linear);
 
-        Toast.makeText(this,content.getContenido().toString(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, content.getContenido().toString(), Toast.LENGTH_SHORT).show();
         Toast.makeText(this, content.getTematicas().toString(), Toast.LENGTH_SHORT).show();
         Toast.makeText(this, Integer.valueOf(content.getExtraIndiceTematica()).toString(), Toast.LENGTH_SHORT).show();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //probar descarga audio
-                try {
-                    String path;
-                    path=filesManage.writeAudio( server.getAudio("000.aac"),getApplicationContext(),"000.aac");
-                    showAudio(path);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }).start();
-
-
 
     }
 
-    private void showAudio(String nombre) throws IOException {
-        View view = new View(this);
-        AudioPlayer audio = new AudioPlayer(view);
-        audio.setAudioUri(Uri.parse(nombre));
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        view.setLayoutParams(params);
-        layout.addView(view);
-        audio.start();
-    }
+
 }
--- app/src/main/java/com/example/alejandro/esmus/ShowAdviceActivity.java
+++ app/src/main/java/com/example/alejandro/esmus/ShowAdviceActivity.java
@@ -1,13 +1,26 @@
 package com.example.alejandro.esmus;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.alejandro.esmus.vista.AudioPlayer;
+import com.example.alejandro.esmus.vista.ProgressTask;
+
+import java.io.IOException;
 
 public class ShowAdviceActivity extends ModelActivity {
+
+
+    private LinearLayout layout;
+    private String path="hola";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,44 @@ public class ShowAdviceActivity extends ModelActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        new ProgressTask<String>(this){
+
+
+            @Override
+            protected String  work() throws Exception {
+
+                path= filesManage.writeAudio( server.getAudio("000.aac"),getApplicationContext(),"000.aac");
+                Log.e("esmus", path);
+
+                return path;
+
+            }
+
+            @Override
+            protected void onFinish(String  result) {
+
+                try {
+                    showAudio(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }.execute();
     }
 
+    private void showAudio(String nombre) throws IOException {
+        View view = new View(this);
+        AudioPlayer audio = new AudioPlayer(view);
+        audio.setAudioUri(Uri.parse(nombre));
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        view.setLayoutParams(params);
+        layout.addView(view);
+        audio.start();
+    }
 }
+
+
