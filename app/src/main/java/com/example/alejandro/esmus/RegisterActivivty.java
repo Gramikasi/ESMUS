9c19b425b02cbec458237668cb646d1e8cd55d30
Alejandro <alumno@U109021>
2016-01-15 15:43:23 +0100

    Arreglado conflictos de codigo

--- app/src/main/java/com/example/alejandro/esmus/RegisterActivivty.java
+++ app/src/main/java/com/example/alejandro/esmus/RegisterActivivty.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 public class RegisterActivivty extends ModelActivity {
 
     private LinearLayout layout;
-
+////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
