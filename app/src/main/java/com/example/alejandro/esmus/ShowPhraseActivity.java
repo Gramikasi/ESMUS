d9773a24394b5eba52369ca239ad115ce828da34
Alejandro <adeba001@ikasle.ehu.eus>
2016-01-25 18:26:21 +0100

    Arreglando merge(Conflicto)

--- app/src/main/java/com/example/alejandro/esmus/ShowPhraseActivity.java
+++ app/src/main/java/com/example/alejandro/esmus/ShowPhraseActivity.java
@@ -13,27 +13,37 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.alejandro.esmus.presentation.Content;
 import com.example.alejandro.esmus.vista.AudioPlayer;
+import com.example.alejandro.esmus.vista.ListAdapter;
 import com.example.alejandro.esmus.vista.ProgressTask;
-
 import org.json.JSONArray;
 import org.json.JSONException;
-
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import static android.widget.Toast.*;
+
 
 public class ShowPhraseActivity extends ModelActivity {
-
 
     private LinearLayout layout;
     private String path;
     private static final int  AUDIO_REQUEST_CODE = 1;
     private Uri uri;
+    String nombreF;
+    private AudioPlayer audio=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,42 +52,105 @@ public class ShowPhraseActivity extends ModelActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final String nombreF=String.valueOf(content.getExtraIndiceTematica())
+                +String.valueOf(content.getExtraIndiceRegistro())
+                +String.valueOf(content.getExtraIndiceFrase())+".aac";
 
-        new ProgressTask<String>(this){
+        TextView textView=(TextView)findViewById(R.id.phrase_texto);
+        textView.setText("Frase y Traducción");
 
+        ListView listView=(ListView)findViewById(R.id.listViewShowPhrases);
+        ArrayList<String> frases=new ArrayList<>();
+        try {
+            frases.add(0,content.getFrase());
+            frases.add(1,content.getTraduccion());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
+        ListAdapter adapter=new ListAdapter(this.getApplicationContext(),frases);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            protected String  work() throws Exception {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-               path= filesManage.writeAudio(server.getAudio("000.aac"),
-                        Environment.getExternalStorageDirectory().getAbsolutePath(),"000.aac");
+            }
+        });
+        layout = (LinearLayout) findViewById(R.id.showPharase_activity_linear);
 
+        final File dir = new File(Environment.getExternalStorageDirectory() + "/ESMUS/");
+        final String string =Environment.getExternalStorageDirectory() + "/ESMUS/"+
+                String.valueOf(content.getExtraIndiceTematica())
+                +String.valueOf(content.getExtraIndiceRegistro())
+                +String.valueOf(content.getExtraIndiceFrase())+".aac";
 
-                return path;
+        try {
+
+            if(!server.isExist(content.getPath(),string) ){
+
+                new ProgressTask<String>(this){
+
+                    @Override
+                    protected String  work() throws Exception {
+
+                        Log.e("esmus","descargando audio");
+
+                        path= filesManage.writeAudio(server.getAudio(nombreF),
+                                dir.getAbsolutePath(),nombreF);
+
+                        Log.i("esmus",nombreF);
+
+                        return path;
+                    }
+
+                    @Override
+                    protected void onFinish(String  result) {
+
+                        try {
+
+                            content.guardarPathDescarga(path);
+                            filesManage.writeJson(content.getContenido(),
+                                    context.getApplicationContext().openFileOutput("dataFile.json", Context.MODE_PRIVATE));
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.execute();
+
+            }else{
+                path=content.getPath();
+                Log.e("esmus","audio descargado de antes");
 
             }
 
-            @Override
-            protected void onFinish(String  result) {
-
-                try {
-                    showAudio(path);
-                    content.guardarPathDescarga(path);
-                    filesManage.writeJson(content.getContenido(),
-                            context.getApplicationContext().openFileOutput("dataFile.json", Context.MODE_PRIVATE));
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.execute();
-
-
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
+    public void escucharAudio( View view){
+
+        try {
+            showAudio(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void escucharGrabacion(View view)
+    {
+        try {
+            showAudio(content.getPathG());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
     private void showAudio(String nombre) throws IOException {
         View view = new View(this);
-        AudioPlayer audio = new AudioPlayer(view);
+        audio = new AudioPlayer(view);
         audio.setAudioUri(Uri.parse(nombre));
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
@@ -89,17 +162,18 @@ public class ShowPhraseActivity extends ModelActivity {
     public void recordAudio(View view) {
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)) {
-            Toast.makeText(this, R.string.no_micro, Toast.LENGTH_SHORT).show();
+            makeText(this, R.string.no_micro, LENGTH_SHORT).show();
         } else {
             Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
             if(intent.resolveActivity(getPackageManager()) != null){
+                Button button= (Button) findViewById(R.id.button3_showPhare);
+                button.setVisibility(View.VISIBLE);
                 startActivityForResult(intent, AUDIO_REQUEST_CODE);
 
             } else {
-                Toast.makeText(this, "no tienes aplicacion disponible", Toast.LENGTH_SHORT).show();
+                makeText(this, "no tienes aplicacion disponible", LENGTH_SHORT).show();
             }
         }
-
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -108,7 +182,7 @@ public class ShowPhraseActivity extends ModelActivity {
             switch (requestCode) {
                 case  AUDIO_REQUEST_CODE:
 
-                            path=data.getDataString();
+                    path=data.getDataString();
                     try {
                         showAudio(path);
                         content.guardarPathGrabado(path);
@@ -117,15 +191,37 @@ public class ShowPhraseActivity extends ModelActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                     break;
             }
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app_activity_actions, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                startModelActivity(MainActivity.class);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public void onBackPressed()
+    {
+        if(audio.isPlaying()){
+            audio.pause();
+            super.onBackPressed();
+        }
 
+    }
 
 }
-
-
